4447b89b22e422c8ed2fa06460f73d1793574d42
Chris Ngan <chris.ngan24@gmail.com>
2014-02-26 13:56:38 -0500

    browsing functionality

--- public/bin/widget.js
+++ public/bin/widget.js
@@ -11,6 +11,9 @@ $.mortar_data.widgets = $.mortar_data.widgets || {};
 $.mortar_data.widgets.draw_table = function(table_header, table_body, array){
   var key_array = [];
   if(array.length > 0){
+    $(table_header).empty();
+    $(table_body).empty();
+
     $(table_header).append('<tr></tr>');
     for(key in array[0]){
       key_array.push(key);
--- public/js/browse/browse.js
+++ public/js/browse/browse.js
@@ -1,21 +1,30 @@
 (function(){
+  var current_index = 1;
   /*  Controller Functions */
   function init_browse(){
-    $.mortar_data.api.get_browse(
-          get_browse_by(), //quantity 
-          1, //index to start browse
-          '', //directory to browse by 
-          reload_table,
-          function(){
-            console.log('error');
-          }
-      );  
+
+    $('#browse_next_page').on('click', fire_next_page);
+    $('#browse_previous_page').on('click', fire_previous_page);
+    get_browse();
   }
-  
+
+  function get_browse(){
+    $.mortar_data.api.get_browse(
+      get_browse_by(), //quantity 
+      current_index, //index to start browse
+      '', //directory to browse by 
+      fire_reload_table,
+      function(){
+        console.log('error');
+      }
+    ); 
+  }
+
+
   /*
    * data - expect to be an object where each key leads to an array
    */
-  function reload_table(data){
+  function fire_reload_table(data){
     var data_parsed = JSON.parse(data);
     var largest_array = []; //only one array will have content, so pick the largest
     var count = 0;
@@ -26,11 +35,33 @@
         largest_array = curr_array;
       }
     }
+    current_index += largest_array.length; //update current index
     $.mortar_data.widgets.draw_table(
               '#browse_table_header', 
               '#browse_table_body', 
               largest_array
           );
+    console.log(current_index);
+    if(current_index > (get_browse_by() + 1))
+      $('#browse_previous_page').removeAttr('disabled');
+    else
+      $('#browse_previous_page').attr('disabled', 'disabled');
+      
+      
+      
+  }
+
+
+  /*
+   *
+   */
+  function fire_next_page(){
+    get_browse(); 
+  }
+  
+  function fire_previous_page(){
+    current_index -= (get_browse_by()*2);
+    get_browse(); 
   }
 
   function get_browse_by(){
