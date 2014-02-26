1601db5ce6689dfaa1a10eb00ea11b5071ac2300
Chris Ngan <chris.ngan24@gmail.com>
2014-02-26 10:22:44 -0500

    added mortar-table, draws header

--- public/bin/browse.js
+++ public/bin/browse.js
@@ -1,11 +1,12 @@
 (function(){
   /*  Controller Functions */
   function init_browse(){
-     
+    get_browse_results(100, 1, '', reload_table);  
   }
 
-  function reload_table(){
-  
+  function reload_table(data){
+    var data_parsed = JSON.parse(data);
+    $.mortar_data.widgets.draw_table('#browse_table_header', '#browse_table_body', data_parsed.item_item_recs);
   }
 
   function get_browse_by(){
@@ -25,7 +26,7 @@
   }
 
   $(document).ready(function()  {
-       
+      init_browse(); 
   });
 
 
